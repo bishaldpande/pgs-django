41e12df81d0431d611a0e15119d0c02182ac8c63
bishaldpande <bishaldpande@gmail.com>
2024-10-07 09:46:34 +0545

    commit files

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from project.settings import AUT
 
 
 class Blog(models.Model):
@@ -8,6 +9,18 @@ class Blog(models.Model):
     link = models.CharField(max_length=255, blank=True, null=True)
     date_created = models.DateTimeField(auto_now_add=True)
     date_updated = models.DateTimeField(auto_now=True)
+    is_delete = models.BooleanField()
 
     def __str__(self):
         return self.title
+    
+    def delete(self):
+        self.title = ""
+        self.content = "delete post"
+        self.author = None
+        self.save()
+
+class Comment(models.Model):
+    text = models.TextField()
+    blog = models.ForeignKey(Blog, on_delete=models., null=True)
+    # see about many to many field
